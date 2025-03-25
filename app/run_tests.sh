e2ba05e08169da5c8b2a924cb711278529b783ed
Sushama Shroff <sushroff@cisco.com>
2025-03-25 11:34:51 -0700

    fix lint errors in scripts

--- app/run_tests.sh
+++ app/run_tests.sh
@@ -1,5 +1,3 @@
-set -e
-
 #python3 -m venv myenv
 #. myenv/bin/activate
 #
@@ -9,7 +7,7 @@ set -e
 # The purpose of this test is to do a quick validation for the user prompts related to projects.
 # This test is run using a LLM and against an actual Jira instance.
 # USAGE: ./run_tests.sh
-# PREREQUISITES: Required env variables must be specified.
-# NOTE: These tests run updates on the Jira Project. Please use a test jira project for this test.
-source tests/.env
+# PREREQUISITES: Required env variables must be specified in .env based on the .env.sample file.
+# NOTE: These tests *run updates* on the Jira Project. Please use a *test jira project* for this test.
+# source tests/.env
 python3 -m unittest tests.test_prompts_projects
