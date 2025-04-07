0bb19300a48bbeb763f7f2b12962a668fc9d2369
Sri Aradhyula <sraradhy@cisco.com>
2025-04-07 11:43:48 -0500

    fix: address PR comments
    Signed-off-by: Sri Aradhyula <sraradhy@cisco.com>

--- src/agents/issues_agent/tools/__init__.py
+++ src/agents/issues_agent/tools/__init__.py
@@ -31,22 +31,6 @@ TOOLS: List[Callable[..., Any]] = [
   search_jira_issues_using_jql
 ]
 
-TOOLS_A: List[Callable[..., Any]] = [
-  create_jira_issue,
-  assign_jira,
-  update_issue_reporter,
-]
-
-TOOLS_B: List[Callable[..., Any]] = [
-  add_new_label_to_issue,
-  get_jira_issue_details,
-  perform_jira_transition,
-  get_jira_transitions,
-  search_jira_issues_using_jql,
-]
-
-FINAL_TOOL = TOOLS_A if os.getenv("ENV") == "A" else TOOLS_B
-
 __all__ = [
   "_get_account_id_from_email",
   "_create_jira_urlified_list",
